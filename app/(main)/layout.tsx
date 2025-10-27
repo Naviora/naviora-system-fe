0581123419c33cbf902fb96fe757d4952272bfbd
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-10-27 21:02:52 +0700

    feat: new layout meeting screen with dock

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,24 +1,34 @@
+'use client'
+
 import { AppSidebar } from '@/components/layouts/app-sidebar'
 import { Navbar } from '@/components/common/navbar'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AuthGuard } from '@/components/guard/auth-guard'
+import { usePathname } from 'next/navigation'
 
 export default function MainLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const pathname = usePathname()
+  const isMeetingPage = pathname?.includes('/meeting')
+
   return (
     <AuthGuard>
-      <SidebarProvider defaultOpen={false}>
-        <AppSidebar />
-        <div className='w-full py-2 pr-2 bg-greyscale-25'>
-          <div className='flex flex-col h-full rounded-md border-1 bg-greyscale-0'>
-            <Navbar />
-            {children}
+      {isMeetingPage ? (
+        children
+      ) : (
+        <SidebarProvider defaultOpen={false}>
+          <AppSidebar />
+          <div className='w-full py-2 pr-2 bg-greyscale-25'>
+            <div className='flex flex-col h-full rounded-md border-1 bg-greyscale-0'>
+              <Navbar />
+              {children}
+            </div>
           </div>
-        </div>
-      </SidebarProvider>
+        </SidebarProvider>
+      )}
     </AuthGuard>
   )
 }
--- app/(main)/meeting/page.tsx
+++ app/(main)/meeting/page.tsx
@@ -4,10 +4,10 @@ import { Suspense } from 'react'
 
 export default function MeetingPage() {
   return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <WebRTCRoom signalingUrl='http://localhost:3000' />
-      </Suspense>
-    </div>
+    <Suspense
+      fallback={<div className='flex items-center justify-center h-full text-white bg-[#202124]'>Loading...</div>}
+    >
+      <WebRTCRoom signalingUrl='http://localhost:3000' />
+    </Suspense>
   )
 }
