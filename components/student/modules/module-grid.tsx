47ae95db92ff116dd0af1d8d00887c6a43d2e416
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-10-28 16:52:19 +0700

    feat: add module card, grid, and toolbar components for student modules

---
+++ components/student/modules/module-grid.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { ModuleCard } from '@/components/student/modules/module-card'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function StudentModuleGrid({ modules }: { modules: any[] }) {
+  return (
+    <div className='grid gap-4 sm:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4'>
+      {modules.map((course) => (
+        <ModuleCard key={course.id} {...course} />
+      ))}
+    </div>
+  )
+}
+
