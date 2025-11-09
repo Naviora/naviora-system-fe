11017d8493e1faef39f4292bc61cc686b73f9b56
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-11-09 15:05:40 +0700

    feat(navigation): add 'class management' to principal navigation

--- lib/constants/navigation.ts
+++ lib/constants/navigation.ts
@@ -51,7 +51,8 @@ const LECTURER_MAIN_NAVIGATION = navigationItemsSchema.parse([
 
 const PRINCIPAL_MAIN_NAVIGATION = navigationItemsSchema.parse([
   { name: 'Tổng quan', href: '/principal/dashboard', icon: RiHome5Fill },
-  { name: 'Quản lý chuyên đề', href: '/principal/modules', icon: RiBook2Fill }
+  { name: 'Quản lý chuyên đề', href: '/principal/modules', icon: RiBook2Fill },
+  { name: 'Quản lý lớp', href: '/principal/classes', icon: RiGroupFill }
 ])
 
 const ADMIN_MAIN_NAVIGATION = navigationItemsSchema.parse([
