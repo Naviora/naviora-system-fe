295effd5a8c7f87cb224f04037a12bcb1a7066d1
HuyDiCode <nbquochuy.forwork@gmail.com>
2025-11-11 23:48:56 +0700

    refactor: simplify import statements in ReviewedExerciseModal

--- components/lecturer/exams/reviewed-exercise/reviewed-exercise-modal.tsx
+++ components/lecturer/exams/reviewed-exercise/reviewed-exercise-modal.tsx
@@ -2,13 +2,7 @@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogClose } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue
-} from '@/components/ui/select'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { DatePicker } from '@/components/ui/date-picker'
 import React, { useEffect, useMemo, useState } from 'react'
 import { addHours } from 'date-fns'
