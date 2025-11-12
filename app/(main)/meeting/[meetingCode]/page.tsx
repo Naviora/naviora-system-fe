079a548e435e4ff1b08662a9e27fee9c6cbaaea6
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-11-12 08:05:31 +0700

    fix(meeting): update signaling URL in WebRTCRoom to use environment variable for better configuration

--- app/(main)/meeting/[meetingCode]/page.tsx
+++ app/(main)/meeting/[meetingCode]/page.tsx
@@ -4,12 +4,13 @@ import { Suspense } from 'react'
 
 export default function MeetingByCodePage({ params }: { params: { meetingCode: string } }) {
   const meetingCode = params.meetingCode
+  const signalingUrl = process.env.NEXT_PUBLIC_SIGNALING_URL || 'http://localhost:3000'
 
   return (
     <Suspense
       fallback={<div className='flex items-center justify-center h-full text-white bg-[#202124]'>Loading...</div>}
     >
-      <WebRTCRoom signalingUrl='http://localhost:3000' initialRoomId={meetingCode} />
+      <WebRTCRoom signalingUrl={signalingUrl} initialRoomId={meetingCode} />
     </Suspense>
   )
 }
